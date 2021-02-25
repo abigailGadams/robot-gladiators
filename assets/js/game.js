2cfe4092ef68e8f16300f7b0c6334eb9d59d6aeb
Abigail Adams <abigail-adams@gmail.com>
2021-02-24 16:36:33 -0800

    outline for project

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,2 +1,37 @@
-window.alert("This is an alert! JavaScript is running!");
 
+// Game prompts player to name their robot
+var playerName =  window.prompt("What is your robot's name?");
+
+// initialize players robot with health, attack, and money pts
+const healthPts = 100;
+const attackPts = 10;
+const moneyPts = 10;
+
+// Players opponent Roboto is initialized 
+
+
+
+// Game will display " Welcome to Robot Gladiators!"
+
+// prompt the player to fight the round or skip it
+
+// If player chooses to skip 
+    // 10 pts deducted from players robot
+    // game ends
+
+// if player chooses to fight 
+    // The player's robot will attack Roborto, and the player-robot's attack points will be 
+        // deducted from Roborto's health pts.
+    // Game will disp Roboto's remaining health pts
+    // Roborto will attack players robot 
+        // Robortos attack pts deducted from players health pts
+    // Game will display player-robot's remaining health pts
+
+// Game ENDS
+
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+  }
+
+  fight();
